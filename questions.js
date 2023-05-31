41736ad3bea10944ea823b0eeb085ccf6652785f
LucasLiaw <97913575+LucasLiaw@users.noreply.github.com>
2023-05-31 18:13:07 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    question: "Segundo conversamos, a lista era para hoje. a conjunção estabelece a relação de ",
+    answers: [
+      { option: "conformidade", correct: true },
+      { option: "condição", correct: false },
+      { option: "causa", correct: false },
+      
+    ],
+  },
+  {
+    question: "Ela é bem velhinha e vive de renda! a conjunção estabelece relação de ",
+    answers: [
+      { option: "causa", correct: false },
+      { option: "adição", correct: true },
+      { option: "oposição", correct: false },
+    ],
+  },
+  {
+    question: "O estatuto do idoso é necessário porque as pessoas não respeitam os direitos básicos dos cidadãos. a conjunção estabelece relação de?",
+    answers: [
+      { option: "finalidade", correct: false },
+      { option: "adição", correct: false },
+      { option: "causa", correct: true },
+    ],
+  },
+  {
+    question: "À medida que a empatia aumenta, a criminalidade diminui. a conjunção estabelece relação de",
+    answers: [
+      { option: "comparação", correct: false },
+      { option: "concessão", correct: false },
+      { option: "proporcionalidade", correct: true },
+    ],
+  },
+  {
+    question: "Marque a opção que apresenta apenas artigos definidos.",
+    answers: [
+      { option: "uma, a, o, os", correct: false },
+      { option: "uma, umas, um, uns", correct: false },
+      { option: "a, o, os, as", correct: true },
+    ],
+  },
+
+  {
+    question: "_____ Eduarda é casada com _____ homem que trabalha ajudando ______ pessoas no Posto de Saúde.",
+    answers: [
+      { option: "uma, os, las", correct: false },
+      { option: "uma, o, uma", correct: false },
+      { option: "a, um, as", correct: true },
+    ],
+  },
+
+
+
+
+
+
+
+];
